b53d69e240479206ab4c7e8a12f14c11059235c5
yakovkiff <yakov.kiffel@gmail.com>
2017-09-01 13:23:54 -0400

    fetch works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './SearchBar'
 
 class App extends Component {
+
+  getBeer = (beerName) => {
+    // /search?q=Goosinator&type=beer
+    fetch(`http://api.brewerydb.com/v2/search?q=${beerName}&key=eec0feeeee89d3390e84b6b058eeb065&type=beer`)
+    .then(res => res.json())
+    .then(beer => console.log(beer))
+    //
+  }
+
+
+
   render() {
     return (
       <div className="App">
@@ -10,9 +22,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <SearchBar getBeer={this.getBeer}/>
+        </div>
       </div>
     );
   }
